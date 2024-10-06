e891810e2f29cb3e139b5a868d6ed10bf76a5228
john-ld <153510178+john-ld@users.noreply.github.com>
2024-10-06 22:42:12 +0200

    Corection des imports footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 // Importation des styles spécifiques pour le footer
 import "../Footer/Footer.css"
-import logo from "../../assets/logoblack.png"
+import logo from "../../assets/Logoblack.png"
 
 // Définition du composant fonctionnel (Footer)
 const Footer = () => {
   return (
-    <footer className="footer">
-      <img src={logo} alt="logo kasa footer" />
+    <footer className="Footer">
+      <img src={logo} alt="logo kasa Footer" />
       <p className="copyright">
         © 2020 Kasa. All rights reserved
       </p>
